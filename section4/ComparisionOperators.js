fee11c0a4d700f0730d36022b4ebe853dca2318d
ogatalars <ogataogara@gmail.com>
2022-05-16 19:51:53 -0300

    Introdução a comparações

--- section4/ComparisionOperators.js
+++ section4/ComparisionOperators.js
@@ -1,1 +1,16 @@
-// Comparison 
+// // Comparison
+// > // greater than
+// < // less than
+// >= // greater than or equal to
+// <= // less than or equal to
+// == // equal to
+// != // not equal to
+// === // equal to and same type
+// !== // not equal to and same type
+// todos os operadores vao retornar verdadeiro ou falso.
+// Exemplo:
+console.log(1 > 2); // false
+console.log(1 >= 1); // true
+console.log(1 == 1); // true
+console.log("a" > "A"); // false = UTF-8 = > list of unicode characters
+console.log("a" == "A"); // false = ASCII
