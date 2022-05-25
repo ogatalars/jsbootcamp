9128101b2034467229d3be5ff6582e73ad01207d
ogatalars <ogataogara@gmail.com>
2022-05-24 22:34:21 -0300

    Introdução a objetos

---
+++ section6/introObjects.js
@@ -0,0 +1,18 @@
+// Uma forma de data estrutura = : Object
+// Objetos são coleções de propriedades e métodos..
+// Propiedade são a chave e o valor
+// Rather than accessing data using an index, we use custom keys
+// não necessarimante tem uma ordem
+// Vale dizer que propriedade = Chave seguida de valor
+// property = key + value
+// objetos na verdade são chamadas de dictionários
+// objetos são coleções de pares chave/valor
+// objetos são containers que contém informação em forma de par chave/valor
+
+const fitBitData = {
+  totalSteps: 30540,
+  totalMiles: 33.1,
+  avgCalories: 2322,
+  workOutsThisWeek: "5 of 6",
+  avgGoodSleep: "7.2 hours",
+};
